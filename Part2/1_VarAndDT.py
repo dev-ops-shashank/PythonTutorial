876cec920328b33297f00b01d31599dc7e8332c1
dev-ops-shashank <pandeyshashank8585@gmail.com>
2024-09-02 12:58:50 +0530

    Added 3 new files

---
+++ Part2/1_VarAndDT.py
@@ -0,0 +1,21 @@
+# We will Learn Variables & data types in this part
+# Variable is the name given to a memory location in a program, Example:
+var1 = 32
+var2 = "Python"
+var3 = 4.66
+var4 = True
+
+# Data types are the definition of a variable. Example:
+print(type(var1))   # return the data type of Var1 = <class 'int'>
+print(type(var2))   # return the data type of Var1 = <class 'str'>
+print(type(var3))   # return the data type of Var1 = <class 'float'>
+print(type(var4))   # return the data type of Var1 = <class 'bool'>
+
+# There are Primarily 4 types of Data that we use most of the time. They are Integer, Float, String, Boolean.
+# There are other data types also but we will learn about them later in this Tutorial
+
+# Rules for these data types:
+# Integer only contains tru postive & negative values
+# Float contains all the decimals values
+# String contains all the values within double quotes ""
+# Boolean only have 2 values - True & False, without any quotes
---
+++ Part2/2_OperatorsInPython.py
@@ -0,0 +1,12 @@
+# Below are the Mathematical Operators used in Python
+# Arithmetic Operators : -, +, *, /, //, **, etc
+# Assignment Operators : =, +=, -=, *=, /=, etc
+# Comparison Operators : ==, >=, <=, <, >, !=, etc
+# Logical Operators : and, or, not
+print(12+77)    #prints 89
+print(12/5)     #prints 2.4, returns float number
+print(12//5)    #prints 2, return integer after removing all the numbers after decimal in result. note: this does not do the roundOff
+print(3*2)      #prints 6, simply returns the multiple
+print(3**2)     #prints 9, returns 3 to the power 2, Exponential
+
+# In next SubPart, we will create a small Calculator using these operators.
