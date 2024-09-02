2f74ac575f79e636425c0c0e176af0d3d885df91
dev-ops-shashank <pandeyshashank8585@gmail.com>
2024-09-02 13:12:56 +0530

    Modified one file

--- Part2/2_OperatorsInPython.py
+++ Part2/2_OperatorsInPython.py
@@ -9,4 +9,23 @@ print(12//5)    #prints 2, return integer after removing all the numbers after d
 print(3*2)      #prints 6, simply returns the multiple
 print(3**2)     #prints 9, returns 3 to the power 2, Exponential
 
+# type casting: converting one type to another if the value is of the new type only
+# Ex: to convert INT type to Float, we can do that by :
+x=10
+y = float(x)
+print(type(x))
+print(type(y))
+
+# but if we try to convert string to integer, it will give us error:
+# a = "sha"
+# b = int(a)  # ValueError: invalid literal for int() with base 10: 'sha'
+# print(type(a))
+# print(type(b))
+
+# below will work as we are converting a float value, which is inside a str variable, to a float integer variable
+a = "31.5"
+b = float(a)  # this will work fine
+print(type(a))
+print(type(b))
+
 # In next SubPart, we will create a small Calculator using these operators.
