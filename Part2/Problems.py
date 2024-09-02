ed06d91094a989f1e7265794bf2f2b28b11e3d74
dev-ops-shashank <pandeyshashank8585@gmail.com>
2024-09-02 13:35:18 +0530

    added a problems file

---
+++ Part2/Problems.py
@@ -0,0 +1,19 @@
+# 1. Write a pyhton program to find reminder when a number is divided by x
+# num = int(input("Enter a number: "))
+# x = int(input("Divide this number by?: "))
+# rem = num % x
+# print(f"Reminder when {num} is divided by {x} is : {rem}")
+
+
+
+# 2. Write a python program to find an average of two numbers entered by user
+# num1 = int(input("Enter Number 1 : "))
+# num2 = int(input("Enter Number 2 : "))
+# print(f"Average of {num1} and {num2} is : {(num1+num2)/2}")
+
+
+
+# 3. Write a python program to find the square of a number
+# num =  int(input("Enter a number : "))
+# square = num **2 
+# print(f"Square of {num} is : {square}")
