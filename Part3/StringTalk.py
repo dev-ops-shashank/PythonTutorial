9e9f1560e90af260fe8eba7f870d333f00cf78b7
dev-ops-shashank <pandeyshashank8585@gmail.com>
2024-09-04 10:14:47 +0530

    added Part 3

---
+++ Part3/StringTalk.py
@@ -0,0 +1,5 @@
+# String is a sequence of character enclosed in quotes.
+# We can write String in 3 ways:
+str1 = 'String 1'       # single quote
+str2 = "String 2"       # double quote
+str3 = '''String 3'''   # triple quote
